8749253cbd9b6978535227c165184f7e284fc586
Hyago Rodrigues <hyago-rodrigues@live.com>
2022-07-02 16:06:05 -0300

    feat: representando itens dinamicamente

--- expenses-chart-component-main/script.js
+++ expenses-chart-component-main/script.js
@@ -10,15 +10,32 @@ const data = fetch('./data.json')
 
 let chartContainer = document.querySelector('.chart-container');
 
+
+
+
 data.then(data => {
-    data.forEach(item => {
+    data.forEach(item => {        
         let chart = document.createElement('div');
         chart.classList.add('chart');
+        const height = item.amount / 8; 
+        let value = "";
+        //maior valor entre os dados
+        let maxValue = Math.max(...data.map(item => item.amount));
+        if(item.amount === maxValue) {
+            value = `<div class="chart-value active" style="--height:${height}em"></div>`;
+        }else{
+            value = `<div class="chart-value" style="--height:${height}em"></div>`;
+        }
         chart.innerHTML = `
+            <div class="chart-wrapper">
+                ${value}
+            </div>
             <div class="chart-title">${item.day}</div>
-            <div class="chart-value">${item.amount}</div>
         `;
         chartContainer.appendChild(chart);
     });
 });
+
+
+
 
