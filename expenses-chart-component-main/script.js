48a2f530f2389f26e78c7ee06db2944cfeacb668
Hyago Rodrigues <hyago-rodrigues@live.com>
2022-07-02 14:48:57 -0300

    feat: carregando os dados do json

--- expenses-chart-component-main/script.js
+++ expenses-chart-component-main/script.js
@@ -1,1 +1,24 @@
 
+const data = fetch('./data.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        return data;
+    })
+    .catch(error => console.log(error));
+
+
+let chartContainer = document.querySelector('.chart-container');
+
+data.then(data => {
+    data.forEach(item => {
+        let chart = document.createElement('div');
+        chart.classList.add('chart');
+        chart.innerHTML = `
+            <div class="chart-title">${item.day}</div>
+            <div class="chart-value">${item.amount}</div>
+        `;
+        chartContainer.appendChild(chart);
+    });
+});
+
